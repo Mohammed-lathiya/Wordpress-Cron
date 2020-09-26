e33091de0aaead0de0fcc101494d30f0da8e29ad
Mohammed Lathiya <71916101+Mohammed-lathiya@users.noreply.github.com>
2020-09-26 10:45:29 +0530

    Create ml_cron.php

---
+++ ml_cron.php
@@ -0,0 +1,29 @@
+<?php
+
+/* Cron Set For Delete Video */
+
+
+function ml_cron_schedules($schedules){
+    if(!isset($schedules["every_two_hours"])){
+        $schedules["every_two_hours"] = array(
+            'interval' => 7200,
+            'display' => __('Every 2 hours'));
+    }
+    return $schedules;
+}
+add_filter('cron_schedules','ck_cron_schedules');
+
+
+
+if (!wp_next_scheduled('ml_task_hook')) {
+  wp_schedule_event( time(), 'every_two_hours', 'ml_task_hook' );
+}
+add_action ( 'ml_task_hook', 'ml_task_function' );
+
+
+
+function ml_task_function() {
+  /* Code */
+}
+
+?>
