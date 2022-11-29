5f58c85b9c3f5a70a42c7c6be6f4c8737763ebb7
Mohammed Lathiya <71916101+Mohammed-lathiya@users.noreply.github.com>
2022-11-29 17:16:34 +0530

    Update ml_cron.php

--- ml_cron.php
+++ ml_cron.php
@@ -1,5 +1,19 @@
 <?php
 
+/*
+There are 2 types of Cron
+
+1. WP Cron
+
+WP Cron is supported in all WordPress sites by default(Unless turned off manually in the wp-config file). The main disadvantage of WP Cron is that it will trigger only when someone visits the site. For example, If there is no site activity for some time say 10 days, the cron will not be triggered on those 10 days.
+
+2. Server Cron
+
+Server Cron is an alternate to WP Cron. The main advantage of Server Cron is that it is not dependent on site activity. But, Server Cron requires separate configuration. As far as we know, it cannot be configured using a Plugin within the site. It can only be configured from the site’s C-Panel or control panel.
+
+If you want to use WP Cron, then in “Select Cron Type” select “WP Cron”. If you want to run Server cron, select “Server Cron”. Please follow the steps mentioned in the “Server Cron Configuration” section. */
+
+
 /* Cron Run Every 2 hour */
 
 
